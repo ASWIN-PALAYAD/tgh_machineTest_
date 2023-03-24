7f68cad821eed8a11e891201beab9fe253175fd8
aswin <aswinpalayad@ymail.com>
2023-03-24 10:17:26 +0530

    additional feature : clear bookmark added

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import QuotedCards from '../components/QuotedCards';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearBookmark } from '../redux/bookmark';
+
 import './Bookmarks.css';
 
 const Bookmarks = () => {
 
   const dispatch = useDispatch();
-  const {items} = useSelector((state)=> state.bookmark)
+  const { items } = useSelector((state) => state.bookmark)
 
   return (
     <div className='bookmarkPage'>
-    
-  {items.length > 0 ? (items.map((item)=> (
-    <div className="bookmarkContainer" key={item._id}>
-      <QuotedCards item={item} />
+
+      {items.length > 0 && (
+        <button className='clearBookmark' onClick={() => dispatch(clearBookmark())} >CLEAR BOOKMARK</button>
+      )}
+
+      {items.length > 0 ? (items.map((item) => (
+        <div className="bookmarkContainer" key={item._id}>
+          <QuotedCards item={item} />
+        </div>
+      ))) : (
+        <div>
+          <h1 className='emptyBookmark' >No favourite added</h1>
+        </div>
+      )}
     </div>
-  ))):(
-    <div>
-      <h1 className='emptyBookmark' >No favourite added</h1>
-    </div>
-  )}
-  </div>
   )
 }
 
--- src/redux/bookmark.js
+++ src/redux/bookmark.js
@@ -14,10 +14,13 @@ const bookMarkSlice = createSlice({
         removeFromBookmark : (state,action) => {
             state.items = state.items.filter((item)=> item._id !== action.payload)
         },
+        clearBookmark : (state,action) => {
+            state.items = []
+        },
         
 
     }
 })
 
-export const {addToBookmark,removeFromBookmark} = bookMarkSlice.actions;
+export const {addToBookmark,removeFromBookmark,clearBookmark} = bookMarkSlice.actions;
 export default bookMarkSlice.reducer;
