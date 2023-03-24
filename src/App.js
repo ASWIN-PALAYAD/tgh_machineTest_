074077f05373bd25fe7f8b71fb0cc064ad97a08d
aswin <aswinpalayad@ymail.com>
2023-03-24 07:05:11 +0530

    layout and routing complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import {createBrowserRouter,RouterProvider,Outlet} from 'react-router-dom';
+
+import NavBar from './components/NavBar.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './pages/Home.jsx';
+import Bookmarks from './pages/Bookmarks.jsx';
 
 function App() {
+
+  const LayOut = () => {
+    return (
+      <div className='mainContainer' >
+        <NavBar/>
+        <Outlet/>
+        <Footer/>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path : '/',
+      element : <LayOut/>,
+      children : [
+        {path : '/', element : <Home/>},
+        {path : '/bookmarks' , element : <Bookmarks/>}
+      ]
+    }
+  ])
+
+
   return (
     <div>
-      <h1>welcome</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
